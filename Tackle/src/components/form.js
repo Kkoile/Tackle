9d2c934d3efd936d9dd7daa6c01efb69349e2ab6
Kkoile <Nils_Hirsekorn@online.de>
2016-03-14 16:58:58 +0100

    added key to form views

--- Tackle/src/components/form.js
+++ Tackle/src/components/form.js
@@ -18,32 +18,45 @@ class Form extends Component {
   render() {
     var form = this.props.form;
     
-    return (<TouchableHighlight underlayColor={'rgba(220,220,220,0.5)'}key={form.name} onPress={this.onPress} style={[styles.containerView, {
-        height: form.tiles.length*TILE_SIZE, 
-        width: form.tiles[0].length*TILE_SIZE
-      }]}>
+    return (
+      <TouchableHighlight 
+        underlayColor={'rgba(220,220,220,0.5)'}
+        key={form.name} 
+        onPress={this.onPress} 
+        style={[
+          styles.containerView, 
+          {
+            height: form.tiles.length*TILE_SIZE, 
+            width: form.tiles[0].length*TILE_SIZE
+          }
+        ]}
+      >
       <View>
       {form.tiles.map(function(tile, i) {
-        return <View style={styles.innerView}>
-        {tile.map(function(tileEntry, j) {
-          var position = {
-            left: j*TILE_SIZE, 
-            top: i*TILE_SIZE
-          }
+        return(
+          <View 
+            key={form.name+'-'+i}
+            style={styles.innerView}
+          >
+          {tile.map(function(tileEntry, j) {
+            var position = {
+              left: j*TILE_SIZE, 
+              top: i*TILE_SIZE
+            }
 
-          var tileStyles = [
-            styles.tile,
-            {
-              backgroundColor: tileEntry == 0 ? 'transparent' : '#F9F6B2',
-              borderWidth: tileEntry
-            },
-            position
-          ]
-          var key = form.name+'-'+i+'-'+j
-          return <View key={key} style={tileStyles} />
-        })}
+            var tileStyles = [
+              styles.tile,
+              {
+                backgroundColor: tileEntry == 0 ? 'transparent' : '#F9F6B2',
+                borderWidth: tileEntry
+              },
+              position
+            ]
+            var key = form.name+'-'+i+'-'+j
+            return <View key={key} style={tileStyles} />
+          })}
         </View>
-
+        )
       })}
       </View>
       </TouchableHighlight>
