6cd7788d81a67c84469d08abde64334be2632b03
Kkoile <Nils_Hirsekorn@online.de>
2016-04-15 12:21:09 +0200

    changed ip address to real server

--- Tackle/src/constants/connection.js
+++ Tackle/src/constants/connection.js
@@ -2,5 +2,5 @@ import React, {
 	Platform
 } from 'react-native'
 
-export const SERVER_URL = (Platform.OS === 'ios') ? 'http://localhost:8080' : 'http://10.0.2.2:8080'
+export const SERVER_URL = 'http://87.106.19.69:443'//(Platform.OS === 'ios') ? 'http://localhost:8080' : 'http://10.0.2.2:8080'
 export const LOGIN = '/login/'
